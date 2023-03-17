ea34b35dfebc61a8be0621181a02c567f43a58c3
orifyuldashev <orif4ikstream@gmail.com>
2023-03-17 15:41:23 +0500

    salom

--- solutions.js
+++ solutions.js
@@ -5,15 +5,15 @@
 // Output: 40
 
 
-function getSum(n) {
-    let sum = 0;
-    for (let i = 1; i < n; i++) {
-        sum += i * (i + 1);
-    }
-    return sum;
-}
+// function getSum(n) {
+//     let sum = 0;
+//     for (let i = 1; i < n; i++) {
+//         sum += i * (i + 1);
+//     }
+//     return sum;
+// }
 
-console.log(getSum(5));
+// console.log(getSum(5));
 
 
 
@@ -23,21 +23,67 @@ console.log(getSum(5));
 //     Input: let arr = [1589, 31851, 512, 78, 180975];
 // Output: [23, 18, 8, 15, 30]
 
-function getSumDigits(arr) {
-    let result = [];
+// function getSumDigits(arr) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let sum = 0;
+//         let num = arr[i];
+//         while (num > 0) {
+//             sum += num % 10;
+//             num = Math.floor(num / 10);
+//         }
+//         result.push(sum);
+//     }
+//     return result;
+// }
+
+// let arr = [1589, 31851, 512, 78, 180975];
+// console.log(getSumDigits(arr));
+
+
+// 3. Products massivi hamda mahsulotlar id sidan iborat massiv berilgan.
+// Id lar massivi ichidagi mahsulotlar narxlari yi'gindisi getTotalPrice(products, ids) funksiyasi tuzilsin.
+
+// Input: let ids = [1, 4, 8];
+// const products = [
+//     { id: 1, name: 'Bike', price: 100 },
+//     { id: 2, name: 'TV', price: 400 },
+//     { id: 3, name: 'Album', price: 800 },
+//     { id: 4, name: 'Book', price: 600 },
+//     { id: 5, name: 'Phone', price: 500 },
+//     { id: 6, name: 'Computer', price: 1000 },
+//     { id: 7, name: 'Skate', price: 300 },
+//     { id: 8, name: 'Keyboard', price: 200 },
+//     { id: 9, name: 'Bottle', price: 700 },
+// ];
+// // getTotalPrice(ids, products)
+// Output: 900
+
+
+//   3-bosh
+
+
+// 7. Yonidagi qo’shnilarining yig'indisidan iborat yangi massiv qaytaruvchi getCloseSum(arr) funksiya tuzilsin.
+
+// Input: [10, 5, 7, 8, 11, 3, 1, 6];
+// Output: [15, 22, 20, 26, 22, 15, 10, 7]
+
+
+function getCloseSum(arr) {
+    const newArr = [];
     for (let i = 0; i < arr.length; i++) {
         let sum = 0;
-        let num = arr[i];
-        while (num > 0) {
-            sum += num % 10;
-            num = Math.floor(num / 10);
+        for (let j = 0; j < arr.length; j++) {
+            if (j !== i) {
+                sum += arr[j];
+            }
         }
-        result.push(sum);
+        newArr.push(sum);
     }
-    return result;
+    return newArr;
 }
 
-let arr = [1589, 31851, 512, 78, 180975];
-console.log(getSumDigits(arr));
+const arr = [10, 5, 7, 8, 11, 3, 1, 6];
+console.log(getCloseSum(arr));
 
 
